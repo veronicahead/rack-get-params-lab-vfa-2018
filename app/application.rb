bfc3269f2bd8d23f7a8165d60eb4b5fa7e03c094
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-28 01:06:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,8 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        resp.write ""
+        resp.write "Your cart is empty"
+      else
     elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
