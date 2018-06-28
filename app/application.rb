41b8d5826bece18bc8f41d8561c801d0dada0477
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-28 01:06:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-    
+      if @@cart.empty?
+        resp.write ""
     elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
